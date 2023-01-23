82bd5434695667fbd216d171015cb89f7b0f3ebd
Jhonatan Javier Corella Pérez <74806674+JhonatanJavierDev@users.noreply.github.com>
2023-01-23 17:58:29 -0300

    Conexion a la base de datos mysql

--- config/db_config.php
+++ config/db_config.php
@@ -1,3 +1,4 @@
 <?php
-
+//Aca cambias la conexion a la tuya, sea un host o entorno local, 
+//Host, usuario, contraseña y Nombre de la base de datos
 $conexion = mysqli_connect("localhost", "root", "", "Database");

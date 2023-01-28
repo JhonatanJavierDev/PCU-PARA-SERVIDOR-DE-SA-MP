124d06e61e0d5aa7c8db956aca79c448b504b933
Jhonatan Javier Corella Pérez <74806674+JhonatanJavierDev@users.noreply.github.com>
2023-01-27 22:48:38 -0300

    Código de validación v2
    Se aplicaron cambios de seguridad en el esquema del código, sanando las vulnerabilidades del código

--- validar.php
+++ validar.php
@@ -1,41 +1,39 @@
 <?php
-//Conexion a nuestra base de datos
+  /*V2 Se aplicarón correciones de seguridad en el código*/
+  //Conexion a nuestra base de datos
   session_start();
   include('./config/db_config.php');
 
-  $name = $_POST['name'];
-  $password = $_POST['password'];
-  
-  $_SESSION['name']=$name;
+  //Se aplico una correción para mejorar la seguridad sanando las vulnerabilidades
+  $name = mysqli_real_escape_string($conexion, $_POST['name']);
+  $password = mysqli_real_escape_string($conexion, $_POST['password']);
 
-
+   
+  $_SESSION['name'] = htmlspecialchars($name, ENT_QUOTES, 'UTF-8');
   
-  if(isset($name) && isset($password)){
-    $cache = mysqli_query($conexion, "SELECT salt FROM player WHERE name = '$name';");
-    if(mysqli_num_rows($cache)>0){
-        $result = mysqli_fetch_array($cache);
-        $salt = $result['salt'];
+ if(isset($name) && isset($password)){
+    $stmt = mysqli_prepare($conexion, "SELECT salt FROM player WHERE name = ?");
+    mysqli_stmt_bind_param($stmt, "s", $name);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_bind_result($stmt, $salt);
+    if(mysqli_stmt_fetch($stmt)){
         $hash = "$password$salt";
         $password = hash('sha256', $hash);
-        $consulta="SELECT*FROM player where name='$name' and pass='$password'";
-        $resultado=mysqli_query($conexion,$consulta);
-        $rows=mysqli_num_rows($resultado);
-        if($rows){
-            header("location:home.php");
-        }else{
+        $stmt = mysqli_prepare($conexion, "SELECT * FROM player WHERE name = ? AND pass = ?");
+        mysqli_stmt_bind_param($stmt, "ss", htmlspecialchars($name), $password);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_store_result($stmt);
+        if(mysqli_stmt_num_rows($stmt) > 0) {
+            header("location: home.php");
+        } else {
             include("index.html");
         }
-    }else{
+    } else {
         include("index.html");
     }
-}else{
+    mysqli_stmt_close($stmt);
+} else {
     include("index.html");
 }
 
-if (isset($resultado)) {
-    mysqli_free_result($resultado);
-}
-if (isset($resultado)) {
-    mysqli_free_result($resultado);
-}
 mysqli_close($conexion);

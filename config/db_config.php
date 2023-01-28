fc1c0931757f8ba06a2d6c82dc83b26542cda33c
Jhonatan Javier Corella Pérez <74806674+JhonatanJavierDev@users.noreply.github.com>
2023-01-27 22:52:30 -0300

    db_config.php V2

--- config/db_config.php
+++ config/db_config.php
@@ -2,3 +2,29 @@
 //Aca cambias la conexion a la tuya, sea un host o entorno local, 
 //Host, usuario, contraseña y Nombre de la base de datos
 $conexion = mysqli_connect("localhost", "root", "", "Database");
+
+// Preparamos la conexión para usar sentencias preparadas
+$conexion->set_charset("utf8");
+$conexion->prepare = true;
+
+// Aqui ejecutamos las consultas preparadas
+function ejecutar_consulta($conexion, $query, $parametros) {
+    // Preparar la consulta
+    $stmt = $conexion->prepare($query);
+    // Vinculamos los parámetros
+    call_user_func_array(array($stmt, 'bind_param'), refValues($parametros));
+    // Se hace un execute de la consulta
+    $stmt->execute();
+    return $stmt;
+}
+
+// Esta Función es  auxiliar el vinculo de parámetros
+function refValues($arr) {
+    if (strnatcmp(phpversion(), '5.3') >= 0) {
+        $refs = array();
+        foreach ($arr as $key => $value) {
+            $refs[$key] = & $arr[$key];
+        }
+        return $refs;
+    }
+    return $arr;
